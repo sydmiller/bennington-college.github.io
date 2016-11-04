32ba44b33faed1291ed493b2e4e55ef9e7f3b1d9
sydmiller <sydneymiller@bennington.edu>
2016-11-04 10:50:38 -0400

    a

--- assets/js1.js
+++ assets/js1.js
@@ -1,9 +1,20 @@
 $( document ).ready(function() {
 
-	$('h3').mouseover(
+	$('h3').hover(
 	  function() {
 	    $('h3').css('background-color', '#f3ff95');
 	  }
 	);
+	
+	$( "a" ).click(
+	  function() {
+    	$( this ).fadeOut( 1000, function() {
+    	$( "a" ).hide("slow");
+		$( "a" ).click(
+		  function( event ) {
+  			event.preventDefault();
+  			$( this ).hide("slow");
+		  });
+	  });
 
 });
