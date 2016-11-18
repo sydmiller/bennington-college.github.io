412ace6f2b98fde36fcdcd592802ee2b488cd7f1
sydmiller <sydneymiller@bennington.edu>
2016-11-18 08:19:24 -0500

    colors

--- assets/js1.js
+++ assets/js1.js
@@ -1,46 +1,70 @@
+var setFontColor = function(lightValue) {	
+	if (lightValue > 50) {
+	  $('body').css('color', 'black');
+	  $('a').css('color', 'black');
+
+	}
+	else { 
+	  $('body').css('color', 'white');
+	  $('a').css('color', 'white');
+	} 
+}
+
+
 $( document ).ready(function() {
 // hover h3 elements yellow
 	$('h3').mouseenter(
 	  function() {
-	    $('h3').css('background-color', '#f3ff95').fadein(800);
+	    $('h3').css('background-color', '#f3ff95').fadeIn(800);
 	  }
 	);
 
 	$('h3').mouseout(
 	  function() {
-	    $(this).css('background-color', '#f2f2f2').fadein(800);
+	    $(this).css('background-color', '#f2f2f2').fadeIn(800);
 	  }
 	);
 
 	$('button').mouseenter(
 	  function() {
-	  	  var hueValue = Math.Random() * 360;
-		  $('body').css('background-color', 'hueValue');
+	  	  var hueValue = Math.random() * 360;
+	  	  var saturationValue = Math.random() * 100;
+	  	  var lightnessValue = Math.random() * 100;
+	  	  var hsl = "hsl(" + hueValue + "," + saturationValue + "%," + lightnessValue + "%)";
+		  $('body').css('background-color', hsl);
+		setFontColor(lightnessValue);
 	  }
 	);
 
-	$(document).click(
-		function(event) {
-		  	var yPos = event.pageY;
-		  	var xPos = event.pageX;
+	// $(document).click(
+	// 	function(event) {
+	// 	  	var yPos = event.pageY;
+	// 	  	var xPos = event.pageX;
 
-			var screenHeight = $(document).height();
-			var screenWidth = $(document).width();
+	// 		var screenHeight = $(document).height();
+	// 		var screenWidth = $(document).width();
 
-			var satValue = horPos / screenWidth * 100;
-			var lightValue = verPos / screenHeight * 100;
+	// 		var satValue = xPos / screenWidth * 100;
+	// 		var lightValue = yPos / screenHeight * 100;
 
-			if (lightValue < 50) {
-			  $('body').css('font-color', 'black');
+	// 		setFontColor(lightValue);
 
-			}
-			else { 
-			  $('body').css('font-color', 'white');
-			} 
-		}
-	);
-}
-/*/shift body right on hover
+	// 	}
+	// );
+});
+
+
+
+
+// var num = 7;
+// // var numStr = "" + num;
+// var numStr = num.toString();
+// var str = "8";
+// var strNum = parseInt(str);
+
+
+/*
+/shift body right on hover
 // links removed on click
 	$('a').click(
 		function( event ) {
@@ -66,4 +90,5 @@ $( document ).ready(function() {
   		$( this ).toggleClass( "reverse" );
 	});
 
-});
+
+*/
